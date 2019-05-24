2be59c045cf2f460d15a2c8c345d45023f548604
L.R.L <DaleLoreNY@gmail.com>
2019-05-24 12:00:03 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,6 @@
-class Dog
+class Dog < ActiveRecord::Base
 end
+
+
+#literally all you need to do is create the connection to ActiveRecord
+#Review ActiveRecord Mechanics (Link a Student "model" to a database table)
